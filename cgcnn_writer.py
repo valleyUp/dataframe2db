ff72399ea89352424fab58bdd4e0a1cf065c22d7
Glenn <512641455@qq.com>
2023-06-12 16:13:08 +0800

    feat(docker): 增加docker

--- cgcnn_writer.py
+++ cgcnn_writer.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import pymysql
 import json
+import numpy as np
 from monty.json import MontyDecoder, MontyEncoder
 
 conn = pymysql.connect(
@@ -20,24 +21,58 @@ CREATE TABLE IF NOT EXISTS `material_feature` (
     UNIQUE KEY (uni_idx)
 )
 """
-
 sql_insert = "INSERT INTO `material_feature` (material_id, structure, composition) VALUES (%s, %s, %s)"
 
-CGCNN_DATA = '~/Downloads/id_prop_46744.json.gz'
-MP_DATA = '~/Downloads/mp_2019_13w.json.gz'
+CGCNN_DATA = '~/id_prop_46740.json.gz'
 
 data = pd.read_json(CGCNN_DATA)
-# data2 = pd.read_json(MP_DATA)
-data.structure = data.structure.apply(json.loads, cls=MontyDecoder)
+toWrite = data[['mp-id', 'structure', 'composition']]
+toWrite = toWrite.to_numpy().tolist()
 cursor = conn.cursor()
 cursor.execute(sql_create_table)
 cursor.close()
 conn.commit()
-for i, mpp_id in enumerate(data['mp-id']):
-    print(f"Inserting {mpp_id} into database...")
-    structure = data['structure'].iloc[i]
-    composition = structure.composition
-    cursor = conn.cursor()
-    cursor.execute(sql_insert, (mpp_id, json.dumps(structure, cls=MontyEncoder), json.dumps(composition, cls=MontyEncoder)))
-    cursor.close()
-    conn.commit()
+
+cursor = conn.cursor()
+cursor.executemany(sql_insert, toWrite)
+cursor.close()
+conn.commit()
+
+sql_create_idx = """
+CREATE TABLE IF NOT EXISTS `cgcnn_index` (
+    uni_idx INT NOT NULL,
+    isin_3402 TINYINT(1),
+    isin_27430 TINYINT(1),
+    isin_46740 TINYINT(1)
+)
+"""
+sql_insert_idx = "INSERT INTO `cgcnn_index` (uni_idx, isin_3402, isin_27430, isin_46740) VALUES (%s, %s, %s, %s)"
+idx_3402 = pd.read_csv('/home/gll/refRepo/cgcnn/data/material-data/mp-ids-3402.csv', header=None)[0]
+idx_27430 = pd.read_csv('/home/gll/refRepo/cgcnn/data/material-data/mp-ids-27430.csv', header=None)[0]
+idx_46744 = pd.read_csv('/home/gll/refRepo/cgcnn/data/material-data/mp-ids-46744.csv', header=None)[0]
+
+idx = data['mp-id']
+idx2Write = {
+    'uni_idx': (np.arange(46740) + 1).tolist(), 
+    '3402': idx.isin(idx_3402), 
+    '27430': idx.isin(idx_27430), 
+    '46744': idx.isin(idx_46744)
+}
+idx2Write = pd.DataFrame(idx2Write).astype(int)
+cursor = conn.cursor()
+cursor.execute(sql_create_idx)
+cursor.close()
+conn.commit()
+cursor = conn.cursor()
+cursor.execute(sql_create_idx)
+cursor.executemany(sql_insert_idx, idx2Write.to_numpy().tolist())
+cursor.close()
+conn.commit()
+
+sql_create_labels = """
+CREATE TABLE IF NOT EXISTS `material_labels` (
+    uni_idx INT NOT NULL,
+    
+    
+)
+"""
